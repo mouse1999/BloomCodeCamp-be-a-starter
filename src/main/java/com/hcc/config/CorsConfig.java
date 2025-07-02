5675931cad01071b314f285ec0dd88e24b7ca0d2
mouse1999 <kufreedward26@gmail.com>
2025-07-02 21:04:42 +0100

    updated corsconfig

--- src/main/java/com/hcc/config/CorsConfig.java
+++ src/main/java/com/hcc/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://your-frontend-domain.com")
+                .allowedOrigins("http://localhost:5173", "https://bloomcamp.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization")
@@ -33,7 +33,7 @@ public class CorsConfig implements WebMvcConfigurer {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.addAllowedOrigin("http://localhost:5173");
-        config.addAllowedOrigin("http://your-frontend-domain.com");
+        config.addAllowedOrigin("https://bloomcamp.onrender.com");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
